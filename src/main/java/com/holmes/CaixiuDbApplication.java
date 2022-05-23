67f068f11137ad0da9f909830b5a7d6be4e89619
Alex <alxdev94@gmail.com>
2022-05-23 22:53:07 +0100

    removing spring mongodb and using mongodb-driver-sync instead

--- src/main/java/com/holmes/CaixiuDbApplication.java
+++ src/main/java/com/holmes/CaixiuDbApplication.java
@@ -1,92 +1,92 @@
-package com.holmes;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.core.MongoOperations;
-
-@SpringBootApplication
-public class CaixiuDbApplication implements CommandLineRunner {
-
-    @Autowired
-    Environment environment;
-
-//    @Value("$name")
-//    private String name;
-
+//package com.holmes;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.core.env.Environment;
+//import org.springframework.data.mongodb.core.MongoOperations;
+//
+//@SpringBootApplication
+//public class CaixiuDbApplication implements CommandLineRunner {
+//
 //    @Autowired
-//    private UserRepository userRepository;
-
-    private MongoOperations mongoOperations;
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(CaixiuDbApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        for(String s : args) {
-            System.out.println(s);
-        }
-//        CaixiuDbApplication caixiuDbApplication = new CaixiuDbApplication();
-//        mongoOperations = new MongoTemplate(
-//            MongoClients.create(Objects.requireNonNull(environment.getProperty("host"))),
-//            Objects.requireNonNull(environment.getProperty("database"))
-//        );
+//    Environment environment;
 //
-//        if(!mongoOperations.collectionExists("Vocabulary")) {
-//            mongoOperations.createCollection("Vocabulary");
+////    @Value("$name")
+////    private String name;
+//
+////    @Autowired
+////    private UserRepository userRepository;
+//
+//    private MongoOperations mongoOperations;
+//
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CaixiuDbApplication.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        for(String s : args) {
+//            System.out.println(s);
 //        }
-////        mongoOperations.dropCollection("Vocabulary");
-//
-//        if(mongoOperations.findById("123", User.class, "Users") == null) {
-//            mongoOperations.insert(new User("123", "ben", LocalDateTime.now()), "Users");
-//        }
-//
-//        File file = new File("src/main/resources/chinese_vocabulary.data");
-//        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-//
-//        List<Vocabulary> vocabDataLoad = new ArrayList<>();
-//        List<Vocabulary> insertVocab = new ArrayList<>();
-//        List<Vocabulary> updateVocab = new ArrayList<>();
-//        List<Vocabulary> findResults = mongoOperations.findAll(Vocabulary.class, "Vocabulary");
-//
-//        while(bufferedReader.ready()) {
-//            String line = bufferedReader.readLine();
-//            if (!line.startsWith("-")) {
-//                String[] items = line.split(",");
-//                vocabDataLoad.add(new Vocabulary(items[0].trim(), items[1].trim()));
-//            }
-//        }
-//
-////        Query query = new Query();
-////        query.addCriteria(criteriaDefinition);
-////        query.addCriteria(
-////                Criteria.where("chinese").is(items[0]).
+////        CaixiuDbApplication caixiuDbApplication = new CaixiuDbApplication();
+////        mongoOperations = new MongoTemplate(
+////            MongoClients.create(Objects.requireNonNull(environment.getProperty("host"))),
+////            Objects.requireNonNull(environment.getProperty("database"))
 ////        );
-//
-//
-//
-//
-//        for (Vocabulary vocabulary : vocabDataLoad) {
-//            if(findResults.contains(vocabulary)) {
-//                System.out.println("Contains " + vocabulary.getChinese());
-//                updateVocab.add(vocabulary);
-//            }
-//            else {
-//                insertVocab.add(vocabulary);
-//            }
-//        }
-//
-//        if(!insertVocab.isEmpty()) {
-//            mongoOperations.insert(insertVocab, "Vocabulary");
-//        }
-//        if(!updateVocab.isEmpty()) {
-////            mongoOperations.upda
-//        }
-    }
-}
+////
+////        if(!mongoOperations.collectionExists("Vocabulary")) {
+////            mongoOperations.createCollection("Vocabulary");
+////        }
+//////        mongoOperations.dropCollection("Vocabulary");
+////
+////        if(mongoOperations.findById("123", User.class, "Users") == null) {
+////            mongoOperations.insert(new User("123", "ben", LocalDateTime.now()), "Users");
+////        }
+////
+////        File file = new File("src/main/resources/chinese_vocabulary.data");
+////        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+////
+////        List<Vocabulary> vocabDataLoad = new ArrayList<>();
+////        List<Vocabulary> insertVocab = new ArrayList<>();
+////        List<Vocabulary> updateVocab = new ArrayList<>();
+////        List<Vocabulary> findResults = mongoOperations.findAll(Vocabulary.class, "Vocabulary");
+////
+////        while(bufferedReader.ready()) {
+////            String line = bufferedReader.readLine();
+////            if (!line.startsWith("-")) {
+////                String[] items = line.split(",");
+////                vocabDataLoad.add(new Vocabulary(items[0].trim(), items[1].trim()));
+////            }
+////        }
+////
+//////        Query query = new Query();
+//////        query.addCriteria(criteriaDefinition);
+//////        query.addCriteria(
+//////                Criteria.where("chinese").is(items[0]).
+//////        );
+////
+////
+////
+////
+////        for (Vocabulary vocabulary : vocabDataLoad) {
+////            if(findResults.contains(vocabulary)) {
+////                System.out.println("Contains " + vocabulary.getChinese());
+////                updateVocab.add(vocabulary);
+////            }
+////            else {
+////                insertVocab.add(vocabulary);
+////            }
+////        }
+////
+////        if(!insertVocab.isEmpty()) {
+////            mongoOperations.insert(insertVocab, "Vocabulary");
+////        }
+////        if(!updateVocab.isEmpty()) {
+//////            mongoOperations.upda
+////        }
+//    }
+//}
--- src/main/java/com/holmes/setup/CaixiuMongoOperations.java
+++ src/main/java/com/holmes/setup/CaixiuMongoOperations.java
@@ -1,20 +1,20 @@
-package com.holmes.setup;
-
-import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-@Configuration
-public class CaixiuMongoOperations {
-
-    private final MongoOperations mongoOperations;
-
-    public CaixiuMongoOperations(String host, String database) {
-        mongoOperations = new MongoTemplate(MongoClients.create(host), database);
-    }
-
-    public MongoOperations getMongoOperations() {
-        return this.mongoOperations;
-    }
-}
+//package com.holmes.setup;
+//
+//import com.mongodb.client.MongoClients;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.MongoOperations;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//
+//@Configuration
+//public class CaixiuMongoOperations {
+//
+//    private final MongoOperations mongoOperations;
+//
+//    public CaixiuMongoOperations(String host, String database) {
+//        mongoOperations = new MongoTemplate(MongoClients.create(host), database);
+//    }
+//
+//    public MongoOperations getMongoOperations() {
+//        return this.mongoOperations;
+//    }
+//}
